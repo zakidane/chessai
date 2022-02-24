6cd32c20365aeeb155fff3d919b19936f81f2f67
zakidane <tekeste223@gmail.com>
2022-02-23 22:00:05 -0500

    initial setup

---
+++ src/main/java/com/mychess/ai/AiApplication.java
@@ -0,0 +1,14 @@
+package com.mychess.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(AiApplication.class, args);
+	}
+
+}
